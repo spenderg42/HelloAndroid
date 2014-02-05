10b4d74c47e535ceb84aa36b4fc3fb1bcafa8bb2
Savannah Pendergrass <spenderg@harding.edu>
2014-02-05 16:12:48 -0600

    Added name and age variables to Person class.

--- HelloAndroid/src/com/example/helloandroid/Person.java
+++ HelloAndroid/src/com/example/helloandroid/Person.java
@@ -1,5 +1,25 @@
 package com.example.helloandroid;
 
 public class Person {
-	//TODO
+	public String name;
+	public int age;
+	
+	public Person()
+	{
+		name = "Savannah";
+		age = 20;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
